b145345ffa49510b5aab52fb71c493c783f1f025
Elias A <athey.elias@gmail.com>
2017-12-03 14:51:23 -0700

    added data sets to res directory. All data sets have had the class label data removed

--- Clustering/src/Driver.java
+++ Clustering/src/Driver.java
@@ -6,16 +6,36 @@ public class Driver {
     /**
      * the clustering algorithm used
      */
-    private Clustering clusteringAlgorithm;
+    private static Clustering clusteringAlgorithm;
 
     /**
      * Clusters a data set using a specific clustering algorithm
-     * @param args
+     * @param args [clustering-algorithm] [num-clusters] [data-set] [parameters]
      */
     public static void main(String[] args){
         if(!Driver.verifyArgs(args)){
+            Driver.displayHelpText();
             System.exit(0);
         }
+
+        switch(args[0]){
+            case "aco":
+                Driver.clusteringAlgorithm = new ACO();
+                break;
+            case "dbs":
+                Driver.clusteringAlgorithm = new DBScan();
+                break;
+            case "km":
+                Driver.clusteringAlgorithm = new KMeans();
+                break;
+            case "nn":
+                Driver.clusteringAlgorithm = new BackpropNN();
+                break;
+            case "pso":
+                Driver.clusteringAlgorithm = new PSO();
+                break;
+        }
+
     }
 
     /**
@@ -25,5 +45,9 @@ public class Driver {
      */
     private static boolean verifyArgs(String[] args){
         return true;
+    }
+
+    private static void displayHelpText(){
+        System.out.print("\nHelp Text\n");
     }
 }
