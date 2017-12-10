b0873460bf2b406822edc3c0f58329176399dbf9
Aaron McCarthy <amccarthy9904@gmail.com>
2017-12-09 20:26:57 -0700

    added comments explaining instance variables

--- Clustering/src/DB_point.java
+++ Clustering/src/DB_point.java
@@ -4,9 +4,9 @@ import java.util.ArrayList;
  * Datapoints in db-scan
  */
 public class DB_point{
-    private final Double[] value;
-    private final int dbLabel;
-    private int cluster;
+    private final Double[] value;   //holds the data values
+    private final int dbLabel;      //0 = noise 1 = border 2 = core
+    private int cluster;            //represents the cluster this point belongs to
 
     public DB_point(Double[] value, int dbLabel){
         this.value = value;
